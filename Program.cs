97a55fdc2d8e6aa726dba10050f8c819e8eb313b
emaan-rehman <247284@students.au.edu.pk>
2026-05-15 09:21:17 +0500

    resolved exception

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddRazorComponents()
 
 // Change this
 builder.Services.AddDbContextFactory<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
 // This line fixes the CS0311 error by using the correct interface/class pair
 builder.Services.AddScoped<ICampusRepository, CampusRepository>();
